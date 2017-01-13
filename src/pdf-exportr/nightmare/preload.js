c2dc612a6089e53a8eb2d39ae979be2ffa33da3f
Paul Esson <paul.esson@blueflag.com.au>
2017-01-13 15:43:16 +1100

    Nightmare preload tests

--- src/pdf-exportr/nightmare/preload.js
+++ src/pdf-exportr/nightmare/preload.js
@@ -4,13 +4,13 @@ __nightmare.ipc = require('electron').ipcRenderer;
 XMLHttpRequest.prototype.outstanding_length = 0;
 var oldSend = XMLHttpRequest.prototype.send;
 XMLHttpRequest.prototype.send = function(){
-    XMLHttpRequest.prototype.outstanding_length++
+    XMLHttpRequest.prototype.outstanding_length++;
     var oldReadyStateChange = this.onreadystatechange;
     this.onreadystatechange = function(){
         if(this.readyState === XMLHttpRequest.DONE){
-            XMLHttpRequest.prototype.outstanding_length--
+            XMLHttpRequest.prototype.outstanding_length--;
         }
         oldReadyStateChange();
-    }
+    };
     oldSend.apply(this, arguments);
-}
+};
